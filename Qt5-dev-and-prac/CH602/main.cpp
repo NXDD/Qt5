77697c309bbd0681a880e152facbebd14fc9de19
NXDD <32304247+NXDD@users.noreply.github.com>
2019-08-07 14:15:38 +0800

    main.cpp

---
+++ Qt5-dev-and-prac/CH602/main.cpp
@@ -0,0 +1,15 @@
+#include "mainwidget.h"
+#include <QApplication>
+#include <QFont>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QFont f("ZYSong18030", 12); //设置字体
+    a.setFont(f);
+
+    MainWidget w;
+    w.show();
+
+    return a.exec();
+}
