b98b0d21f957b9cc9cc08b5754903816eb82b00e
NXDD <32304247+NXDD@users.noreply.github.com>
2019-08-16 17:39:49 +0800

    Create butterfly.h

---
+++ Qt5-dev-and-prac/CH701/Headers/butterfly.h
@@ -0,0 +1,31 @@
+#ifndef BUTTERFLY_H
+#define BUTTERFLY_H
+
+#include <QGraphicsItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QObject>
+
+class Butterfly : public QObject, public QGraphicsItem
+{
+    Q_OBJECT
+public:
+    Butterfly();
+    void timerEvent(QTimerEvent *);
+    QRectF boundingRect() const;
+
+protected:
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+
+private:
+    bool up;
+    QPixmap pix_up;
+    QPixmap pix_down;
+
+    qreal angle;
+
+};
+
+
+#endif // BUTTERFLY_H
